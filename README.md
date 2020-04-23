b8acd008b20a8c915a328c1d0d0984d7ee0af68e
Emmanuel Tweneboah <emmanuel@Emmanuels-MacBook-Pro.local>
2020-04-23 13:16:37 +0800

    Code

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# graphql-complete-guide
-A complete guide to Graphql
+# Setting up babel to use ES6 in node
+
+## Install these packages
+
+i. babel-cli
+ii. babel-preset-env
+
+# Configure your script
+
+```js
+  "scripts": {
+    "start": "babel-node app.js"
+  }
+```
+
+## Test it
+
+In your app.js
+
+```js
+console.log("yea");
+```
+
+# Setting up Graphql Server
---
+++ app.js
@@ -0,0 +1,27 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+//Type Definition
+const typesDefs = `
+type Query {
+    hello: String!
+}
+`;
+
+//Resolvers
+
+const resolvers = {
+  Query: {
+    hello() {
+      return "Hello";
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs: typesDefs,
+  resolvers: resolvers,
+});
+
+server.start(() => {
+  console.log("Graphql Server is running");
+});
